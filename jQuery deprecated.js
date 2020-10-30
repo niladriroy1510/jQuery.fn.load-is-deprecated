67a0551b366e46f06f2bd2fd264536fba2a5c8fc
Niladri Roy <niladriroyweb@outlook.com>
2020-10-30 09:22:48 +0600

    Add files via upload

---
+++ jQuery deprecated.js
@@ -0,0 +1,13 @@
+/* jQuery.fn.load() is deprecated */
+
+/* /wp-content/plugins/wp-analytify/assets/default/js/wp-analytify-dashboard.js: jQuery.fn.load() is deprecated */
+
+
+/* find: */
+
+$(window).load(function() {
+	
+	
+/* 	replace with: */
+
+$(window).on('load',function () {
